7cce6773d012524062d586b84b351a71ee513ad3
fbayanatiDotData <farhad.bayanati@dotData.com>
2021-09-15 17:23:39 -0600

    initial commit

---
+++ apps/dot-data-ng-nx-cloud/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dot-data-us-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dot-data-ng-nx-cloud';
+}
